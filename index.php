bbb536f1a1f6b36689c6609884a634a555ed45d3
Hamdija Salkic <salkic.hamdija@gmail.com>
2024-06-01 16:21:17 +0200

    recent posts

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
 <?php include 'template_parts/header.php'?>
 <?php include 'template_parts/main-header.php'?>
+<?php include 'template_parts/recent-posts.php'?>
 <?php include 'template_parts/footer.php'?>
---
+++ template_parts/recent-posts.php
@@ -0,0 +1,24 @@
+<div class="recent-posts py">
+    <div class="container">
+        <div class="d-flex justify-content-between">
+         <h3>Recent Posts</h3>
+         <a href="">View all</a>
+        </div>
+        <div class="row mt-3">
+          <div class="col-md-6">
+           <div class="wrapper">
+           <h3><a href="">Making a Design System from Scratch</a></h3>
+            <span>12 Feb 2020 &nbsp;&nbsp; | &nbsp;&nbsp; Design Pattern</span>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illo quae facere pariatur ipsa quia saepe, omnis nesciunt veritatis sequi totam fuga temporibus sed vitae, repudiandae quod? Libero, reprehenderit eveniet. Consequatur.</p>
+           </div>
+          </div>
+          <div class="col-md-6">
+            <div class="wrapper">
+            <h3><a href="">Making a Design System from Scratch</a></h3>
+            <span>12 Feb 2020 &nbsp;&nbsp; | &nbsp;&nbsp; Design Pattern</span>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illo quae facere pariatur ipsa quia saepe, omnis nesciunt veritatis sequi totam fuga temporibus sed vitae, repudiandae quod? Libero, reprehenderit eveniet. Consequatur.</p>
+            </div>
+          </div>
+        </div>
+    </div>
+</div>
